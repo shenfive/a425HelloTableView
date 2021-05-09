6ee89ced69c7071b6f61854c1664548bc8853a81
Shen Jun Wu <shenfive@gmail.com>
2021-05-09 10:20:27 +0800

    more TableView

--- a425HelloTableView/ViewController.swift
+++ a425HelloTableView/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     
     
     var contents:[[String:String]] = []
+    var a:[String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         theTableView.dataSource = self
         theTableView.delegate = self
+        
+ 
+    
+        
     }
 
     
@@ -32,21 +38,61 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     }
 
     //MARK: TableViewDelegate
+    
+    /**
+     * 實作TableView方法，自動出現左滑功能
+    **/
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath)
+    {
+        let select:Int = indexPath.row
+        
+        print("hello delete")
+        
+        contents.remove(at: indexPath.row)
+        //=========
+        // 操作
+        //=========
+        tableView.reloadData() // 更新tableView
+    }
+    
+    /**
+     * 自訂delete的文字為刪除
+     */
+    func tableView(_ tableView: UITableView,
+                            titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath)
+            -> String?
+    {
+        return "刪除"
+    }
+    
+    
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header  = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 30))
+        header.text = " 這是 Header "
+        header.backgroundColor = UIColor.yellow
+        
+        return header
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contents.count 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myTableCell") as? MyTableViewCell
-        cell?.headerImg.clipsToBounds = true
-        cell?.headerImg.layer.cornerRadius = 20
-        cell?.phone.text = contents[indexPath.row]["phone"]
-        cell?.name.text = contents[indexPath.row]["name"]
-        return cell ?? UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myTableCell") as! MyTableViewCell
+        cell.headerImg.clipsToBounds = true
+        cell.headerImg.layer.cornerRadius = 20
+        cell.phone.text = contents[indexPath.row]["phone"]
+        cell.name.text = contents[indexPath.row]["name"]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -77,5 +123,19 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
         
     }
 
+    @IBAction func sort(_ sender: Any) {
+        print("start sort")
+        
+        
+        contents.sort { item1, item2 in
+            let name1 = item1["phone"] ?? ""
+            let name2 = item2["phone"] ?? ""
+            return name1 > name2
+        }
+        
+        theTableView.reloadData()
+        
+        
+    }
 }
 
